cc30ecec95afaa4d9153068793e5d58247aa3e37
vicky <vicky64777@gmail.com>
2023-11-20 18:04:38 -0800

    a5 submission

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,16 +1,13 @@
 import axios from "axios";
-const COURSES_URL = "http://localhost:4000/courses";
+const COURSES_URL = "https://kanbas-node-server-app-4cpn.onrender.com/courses";
+const MODULES_URL= "https://kanbas-node-server-app-4cpn.onrender.com/modules";
 
 
-const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
 
 export const updateModule = async (module) => {
   const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
   return response.data;
 };
-
-
 
 export const deleteModule = async (moduleId) => {
     const response = await axios.delete(`${MODULES_URL}/${moduleId}`);

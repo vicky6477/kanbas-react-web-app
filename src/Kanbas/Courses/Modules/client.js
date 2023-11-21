2cf5e6c28f3c3272d069d4cbf4c37f2ddb5a8eb5
vicky <vicky64777@gmail.com>
2023-11-20 17:17:23 -0800

    a5 submission

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,14 +1,15 @@
 import axios from "axios";
-const COURSES_URL = "'http://localhost:4000/modules";
+const COURSES_URL = "http://localhost:4000/courses";
+
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 const MODULES_URL = `${API_BASE}/modules`;
 
 export const updateModule = async (module) => {
-  const response = await axios.
-    put(`${MODULES_URL}/${module._id}`, module);
+  const response = await axios.put(`${MODULES_URL}/${module._id}`, module);
   return response.data;
 };
+
 
 
 export const deleteModule = async (moduleId) => {
